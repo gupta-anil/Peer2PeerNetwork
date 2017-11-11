da87660477ec73162f161d6634f1e3e9ea817ae5
= <=>
2017-11-11 19:32:29 +0530

    More Functionalities added to gui

--- gui.py
+++ gui.py
@@ -19,6 +19,7 @@ class GUI(Frame):
         self.updatePeerFileList()
         self.p2pnetwork.addPeer(1,(host,port))
         self.updatePeerList()
+
         # t = threading.Thread( target = self.p2pnetwork.main, args = [] )
         # t.start()
 
@@ -37,7 +38,7 @@ class GUI(Frame):
             p = self.p2pnetwork.localFileTable[f]
             if not p:
                 p = '(local)'
-            self.fileList.insert( END, "%s:%s" % (f,p) )
+            self.fileList.insert( END, "%s:%s" % (f,p))
     def updatePeerFileList( self ):
         if self.peerfileList.size() > 0:
             self.peerfileList.delete(0, self.peerfileList.size() - 1)
@@ -52,7 +53,7 @@ class GUI(Frame):
       file = self.addfileEntry.get()
       if file.lstrip().rstrip():
          filename = file.lstrip().rstrip()
-         self.btpeer.addlocalfile( filename )
+         self.p2pnetwork.addToLocalFileTable( filename,"./localFiles"+filename )
       self.addfileEntry.delete( 0, len(file) )
       self.updateFileList()
 
@@ -60,10 +61,12 @@ class GUI(Frame):
     def onSearch(self):
       key = self.searchEntry.get()
       self.searchEntry.delete( 0, len(key) )
+      print key
 
-      for p in self.btpeer.getpeerids():
-         self.btpeer.sendtopeer( p, 
-                                 QUERY, "%s %s 4" % ( self.btpeer.myid, key ) )
+      for p in self.p2pnetwork.getPeerIds():
+        print p
+        # self.btpeer.sendtopeer( p, 
+                                 # QUERY, "%s %s 4" % ( self.btpeer.myid, key ) )
 
 
     def onFetch(self):
@@ -72,20 +75,20 @@ class GUI(Frame):
          sel = self.fileList.get(sels[0]).split(':')
          if len(sel) > 2:  # fname:host:port
             fname,host,port = sel
-            resp = self.btpeer.connectandsend( host, port, FILEGET, fname )
+            # resp = self.btpeer.connectandsend( host, port, FILEGET, fname )
             if len(resp) and resp[0][0] == REPLY:
                fd = file( fname, 'w' )
                fd.write( resp[0][1] )
                fd.close()
-               self.btpeer.files[fname] = None  # because it's local now
+               self.p2pnetwork.localFileTable[fname] = "./localFiles"+fname  # because it's local now
 
 
     def onRemove(self):
       sels = self.peerList.curselection()
       if len(sels)==1:
          peerid = self.peerList.get(sels[0])
-         self.btpeer.sendtopeer( peerid, PEERQUIT, self.btpeer.myid )
-         self.btpeer.removepeer( peerid )
+         # self.btpeer.sendtopeer( peerid, PEERQUIT, self.btpeer.myid )
+         self.p2pnetwork.removePeer(peerid)
 
 
     def onRefresh(self):
@@ -143,17 +146,17 @@ class GUI(Frame):
                           command=self.onRefresh)
         self.fetchButton.grid()
 
-        # self.addfileEntry = Entry(addfileFrame, width=25)
-        # self.addfileButton = Button(addfileFrame, text='Add',
-        #                   command=self.onAdd)
-        # self.addfileEntry.grid(row=0, column=0)
-        # self.addfileButton.grid(row=0, column=1)
+        self.addfileEntry = Entry(addfileFrame, width=25)
+        self.addfileButton = Button(addfileFrame, text='Add',
+                          command=self.onAdd)
+        self.addfileEntry.grid(row=0, column=0)
+        self.addfileButton.grid(row=0, column=1)
 
-        # self.searchEntry = Entry(searchFrame, width=25)
-        # self.searchButton = Button(searchFrame, text='Search', 
-        #                   command=self.onSearch)
-        # self.searchEntry.grid(row=0, column=0)
-        # self.searchButton.grid(row=0, column=1)
+        self.searchEntry = Entry(searchFrame, width=25)
+        self.searchButton = Button(searchFrame, text='Search', 
+                          command=self.onSearch)
+        self.searchEntry.grid(row=0, column=0)
+        self.searchButton.grid(row=0, column=1)
 
         peerListFrame = Frame(peerFrame)
         peerListFrame.grid(row=1, column=0)
@@ -166,15 +169,15 @@ class GUI(Frame):
         self.peerList.grid(row=0, column=0, sticky=N+S)
         peerScroll["command"] = self.peerList.yview
 
-        # self.removeButton = Button( pbFrame, text='Remove',
-        #                           command=self.onRemove )
+        self.removeButton = Button( pbFrame, text='Remove',
+                                  command=self.onRemove )
         self.refreshButton = Button( pbFrame, text = 'Refresh', 
                           command=self.onRefresh )
 
-        # # self.rebuildEntry = Entry(rebuildFrame, width=25)
-        # # self.rebuildButton = Button( rebuildFrame, text = 'Rebuild', 
-        #                   # command=self.onRebuild )
-        # self.removeButton.grid(row=0, column=0)
+        # self.rebuildEntry = Entry(rebuildFrame, width=25)
+        # self.rebuildButton = Button( rebuildFrame, text = 'Rebuild', 
+        #                   command=self.onRebuild )
+        self.removeButton.grid(row=0, column=0)
         self.refreshButton.grid(row=0, column=1)
         # self.rebuildEntry.grid(row=0, column=0)
         # self.rebuildButton.grid(row=0, column=1)
